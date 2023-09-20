87e8fd91148db05686d59e3bcf9e48b7c0bc9835
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-09-20 16:41:27 -0300

    feat: Add methods to save new contacts and addresses

--- contacts.js
+++ contacts.js
@@ -3,20 +3,126 @@ angular
   .controller("ContactsController", function ($scope) {
     $scope.currentDate = new Date().toISOString().split("T")[0]
   })
-  .controller("GetAllContacts", function ($scope, $http) {
-    $http({
-      method: "GET",
-      url: "https://www.selida.com.br/avaliacaotecnica/api/Pessoas/GetAll",
-      headers: {
-        "Content-Type": "application/json",
-        Chave: "93E74BA7-2A30-4517-91E1-DDC3DF2558F0"
+
+  .controller("GetAllContacts", function ($scope, ContactService) {
+    ContactService.getAllContacts()
+      .then(function (contacts) {
+        $scope.contacts = contacts
+      })
+      .catch(function (error) {
+        console.error("Erro ao obter contatos:", error)
+      })
+  })
+
+  .controller("CreateContact", function ($scope, ContactService) {
+    $scope.savePerson = function () {
+      const date = new Date($scope.dataNascimento)
+      function formatDate(date) {
+        const year = date.getFullYear()
+        const month = String(date.getMonth() + 1).padStart(2, "0")
+        const day = String(date.getDate()).padStart(2, "0")
+        const hours = String(date.getHours()).padStart(2, "0")
+        const minutes = String(date.getMinutes()).padStart(2, "0")
+        const seconds = String(date.getSeconds()).padStart(2, "0")
+        const milliseconds = String(date.getMilliseconds()).padStart(3, "0")
+
+        return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}`
       }
-    }).then(
-      function successCallback(response) {
-        $scope.contacts = response.data.data
-      },
-      function errorCallback(response) {
-        console.log(response)
+
+      const formattedDate = formatDate(date)
+
+      const personData = {
+        nome: $scope.nome,
+        idade: $scope.idade,
+        dataNascimento: formattedDate,
+        email: $scope.email,
+        telefone: $scope.telefone,
+        celular: $scope.celular
       }
-    )
+
+      console.log(personData)
+
+      ContactService.savePerson(personData)
+        .then(function (personId) {
+          const addressData = {
+            logradouro: $scope.logradouro,
+            cidade: $scope.cidade,
+            bairro: $scope.bairro,
+            numero: $scope.numero,
+            uf: $scope.uf,
+            pessoaId: personId
+          }
+
+          console.log(addressData)
+
+          return ContactService.saveAddress(addressData)
+        })
+        .then(function () {
+          console.log("Pessoa e endereço salvos com sucesso!")
+          $scope.nome = ""
+          $scope.idade = ""
+          $scope.dataNascimento = ""
+          $scope.email = ""
+          $scope.telefone = ""
+          $scope.celular = ""
+          $scope.logradouro = ""
+          $scope.cidade = ""
+          $scope.bairro = ""
+          $scope.numero = ""
+          $scope.uf = ""
+        })
+        .catch(function (error) {
+          console.error("Erro ao salvar pessoa ou endereço:", error)
+        })
+    }
+  })
+  .service("ContactService", function ($http) {
+    this.BASE_URL = "https://www.selida.com.br/avaliacaotecnica/api"
+    this.HEADERS = {
+      "Content-Type": "application/json",
+      Chave: "93E74BA7-2A30-4517-91E1-DDC3DF2558F0"
+    }
+
+    this.getAllContacts = function () {
+      return $http({
+        method: "GET",
+        url: `${this.BASE_URL}/Pessoas/GetAll`,
+        headers: this.HEADERS
+      }).then(function (response) {
+        return response.data.data
+      })
+    }
+
+    this.getContactById = function (id) {
+      return $http({
+        method: "GET",
+        url: `${this.BASE_URL}/Pessoas/${id}`,
+        headers: this.HEADERS,
+        params: {
+          id: id
+        }
+      }).then(function (response) {
+        return response.data.data
+      })
+    }
+
+    this.savePerson = function (personData) {
+      return $http({
+        method: "POST",
+        url: `${this.BASE_URL}/Pessoas`,
+        headers: this.HEADERS,
+        data: personData
+      }).then(function (response) {
+        return response.data.data
+      })
+    }
+
+    this.saveAddress = function (addressData) {
+      return $http({
+        method: "POST",
+        url: `${this.BASE_URL}/Endereco`,
+        headers: this.HEADERS,
+        data: addressData
+      })
+    }
   })
